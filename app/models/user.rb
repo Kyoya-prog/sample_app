c75f1b887dc2602443f61685f2277075a454a0c1
kyoya <kyoyaprog@gmail.com>
2021-06-12 15:33:42 +0900

    永続ログインのシステムを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # メールアドレスを保存の前に小文字に統一しておく（DBによっては大文字小文字を区別できないため、indexのuniqueを通り抜ける恐れがある）
   before_save { self.email = email.downcase }
   validates :name, presence: true,length:{maximum: 50}
@@ -20,5 +21,25 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
              BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #記憶トークンをユーザーと紐付け、トークンに対応するダイジェストをデータベースに保存する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, self.remember_token)
+  end
+
+  #BCriptで暗号化したパスワートダイジェストカラムの値はis_Passwordで認証できる
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest,nil )
   end
 end
