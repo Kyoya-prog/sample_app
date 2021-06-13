afaf23bd55967e61b1cf4c7db37d96dbb5b27f44
kyoya <kyoyaprog@gmail.com>
2021-06-13 10:34:00 +0900

    Userモデルにactivation関連のカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token,:activation_token
   # メールアドレスを保存の前に小文字に統一しておく（DBによっては大文字小文字を区別できないため、indexのuniqueを通り抜ける恐れがある）
   before_save { self.email = email.downcase }
+  before_create{ create_activation_digest }
   validates :name, presence: true,length:{maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,length: {maximum: 255},
@@ -43,5 +44,13 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest,nil )
+  end
+
+  private
+
+  # before_createはユーザーのデータ構造が定義され、データが保存される前に呼び出される
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
