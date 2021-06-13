52f9df94bc1d015c509676a85d3af8ffb6cdd065
kyoya <kyoyaprog@gmail.com>
2021-06-13 15:02:57 +0900

    パスワードの更新とテストを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,6 +68,11 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # パスワード再設定用の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
   # before_createはユーザーのデータ構造が定義され、データが保存される前に呼び出される
