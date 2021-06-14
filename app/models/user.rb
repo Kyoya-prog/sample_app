c87be33f4683e2e464e4afeccf61b41a8ab4ddde
kyoya <kyoyaprog@gmail.com>
2021-06-14 22:33:02 +0900

    フォローされているユーザーとの関連付けを行う

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
            foreign_key: "follower_id",
            dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+           foreign_key: "followed_id",
+           dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers,through: :passive_relationships,source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   # メールアドレスを保存の前に小文字に統一しておく（DBによっては大文字小文字を区別できないため、indexのuniqueを通り抜ける恐れがある）
